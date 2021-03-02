0b943b3aa59014b474ae2565f97720f118881f55
Richelle Soares <richelle.soares@gmail.com>
2021-03-02 20:15:28 -0300

    Finalizando CRUD

--- FormApp.js
+++ FormApp.js
@@ -1,54 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import { Feather as Icon } from '@expo/vector-icons';
 import Database from './Database';
+  
+export default function FormApp({ route, navigation }) {
+  const id = route.params ? route.params.id : undefined;
+  const [descricao, setDescricao] = useState(''); 
+  const [quantidade, setQuantidade] = useState('');
 
-export default function FormApp({route, navigation}) {
-     const id = route.params ? route.params.id : undefined;
-    const [descricao, setDescricao] = useState(''); 
-    const [quantidade, setQuantidade] = useState('');
-    useEffect(() => {
-        if(!route.params) return;
-        setDescricao(route.params.descricao);
-        setQuantidade(route.params.quantidade.toString());
-      }, [route])
- 
-    function handleDescriptionChange(descricao){ setDescricao(descricao); } 
-    function handleQuantityChange(quantidade){ setQuantidade(quantidade); }
-    async function handleButtonPress(){
-        const listItem = {descricao, quantidade: parseInt(quantidade)};
-        Database.saveItem(listItem)
-        .then(response => navigation.navigate("AppList", listItem));
-    }
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Item para comprar</Text>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    onChangeText={handleDescriptionChange}
-                    placeholder="O Que está faltando em casa?"
-                    clearButtonMode="always"
-                    value={descricao}
-                >
-                </TextInput>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Digite a quantidade"
-                    onChangeText={handleQuantityChange}
-                    keyboardType="numeric"
-                    clearButtonMode="always"
-                    value={quantidade.toString()}
-                ></TextInput>
-                <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
-                    <Text style={styles.buttonText}>Salvar</Text>
-                </TouchableOpacity>
+  useEffect(() => {
+    if(!route.params) return;
+    setDescricao(route.params.descricao);
+    setQuantidade(route.params.quantidade.toString());
+  }, [route])
 
+  function handleDescriptionChange(descricao){ setDescricao(descricao); } 
+  function handleQuantityChange(quantidade){ setQuantidade(quantidade); }
+  async function handleButtonPress(){ 
+    const listItem = {descricao, quantidade: parseInt(quantidade)};
+    Database.saveItem(listItem, id)
+      .then(response => navigation.navigate("AppList", listItem));
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Item para comprar</Text>
+      <View style={styles.inputContainer}> 
+        <TextInput 
+          style={styles.input} 
+          onChangeText={handleDescriptionChange} 
+          placeholder="O que está faltando em casa?"
+          clearButtonMode="always"
+          value={descricao} /> 
+        <TextInput 
+          style={styles.input} 
+          onChangeText={handleQuantityChange} 
+          placeholder="Digite a quantidade" 
+          keyboardType={'numeric'}
+          clearButtonMode="always"
+          value={quantidade.toString()} /> 
+          <TouchableOpacity style={styles.button} onPress={handleButtonPress}> 
+            <View style={styles.buttonContainer}>
+              <Icon name="save" size={22} color="white" />
+              <Text style={styles.buttonText}>Salvar</Text> 
             </View>
-            <StatusBar style="auto" />
-        </View>
-    );
+          </TouchableOpacity> 
+      </View>
+      <StatusBar style="light" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -58,10 +59,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   title: {
-      color: "#fff",
-      fontSize: 20,
-      fontWeight: 'bold',
-      marginTop: 40,
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 50,
   },
   inputContainer: {
     flex: 1,
@@ -95,10 +96,13 @@ const styles = StyleSheet.create({
     shadowOpacity: 20,
     shadowColor: '#ccc',
   },
+  buttonContainer: {
+    flexDirection: "row"
+  },
   buttonText: {
+    marginLeft: 10,
+    fontSize: 18,
     color: '#fff',
-    fontSize: 20,
     fontWeight: 'bold',
   }
 });
-
